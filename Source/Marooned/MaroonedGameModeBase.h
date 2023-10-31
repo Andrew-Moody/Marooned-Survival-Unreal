a70063e0ff72f20586f93230a6caf7a9d835747d
Andrew-Moody <78827021+Andrew-Moody@users.noreply.github.com>
2023-10-30 23:55:56 -0500

    Add new project

---
+++ Source/Marooned/MaroonedGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MaroonedGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAROONED_API AMaroonedGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
