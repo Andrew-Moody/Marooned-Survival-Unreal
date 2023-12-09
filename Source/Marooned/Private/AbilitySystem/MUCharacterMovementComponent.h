936435540df8630d6c743552600e49ddcc387a51
Andrew-Moody <78827021+Andrew-Moody@users.noreply.github.com>
2023-12-08 18:19:28 -0600

    Add goblin enemy and powerup shrine - Imported the goblin model and set up locomotion and attack animation - Added a melee attack ability that hits twice for 5 health - Added a shrine actor that spawns a goblin when right clicked   and spawns a health powerup when the goblin is killed

---
+++ Source/Marooned/Private/AbilitySystem/MUCharacterMovementComponent.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "MUCharacterMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UMUCharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+
+public:
+
+	//virtual float GetMaxSpeed() const override;
+};
